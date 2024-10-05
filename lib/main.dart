ac827f35c4c8b7747e9af729fa6d15857c8f20a0
AyaHelmyOraby <129610687+AyaHelmyOraby@users.noreply.github.com>
2024-10-05 11:55:54 +0300

    change theme and create VewView

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
-
 import 'package:flutter/material.dart';
+import 'views/notes_view.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
 class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: NotesView(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
